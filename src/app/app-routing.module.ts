a751b4858d6012e4cf1a94f06417f1efb538d399
EmmanuelPutti <108730976+EmmanuelPutti@users.noreply.github.com>
2023-06-20 13:31:31 +0530

    remove parent child

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'dynamic-loader', 
   loadChildren: () => import('./dynamic-component-loader/dynamic-component-loader.module').then(mod => mod.DynamicComponentLoaderModule) },
-  { path: 'parent-child', 
-  loadChildren: () => import('./parent-child/parent-child-routing.module').then(mod => mod.ParentChildRoutingModule) },
 ];
 
 @NgModule({
